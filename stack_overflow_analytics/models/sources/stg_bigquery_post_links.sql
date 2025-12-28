dd5b93e4d73a55dcbd1def470b1cefa35c7093be
EthanPook10 <Ethan.Pook10@outlook.com>
2025-12-28 08:13:15 +0000

    Added source table for post links to allow for additional insight

---
+++ stack_overflow_analytics/models/sources/stg_bigquery_post_links.sql
@@ -0,0 +1,4 @@
+select id,
+       creation_date,
+       post_id,
+    from `bigquery-public-data.stackoverflow.post_links`
--- stack_overflow_analytics/models/sources/stg_bigquery_tags.sql
+++ stack_overflow_analytics/models/sources/stg_bigquery_tags.sql
@@ -1,2 +1,3 @@
-SELECT id, tag_name
+SELECT id,
+       tag_name
     FROM `bigquery-public-data.stackoverflow.tags`
