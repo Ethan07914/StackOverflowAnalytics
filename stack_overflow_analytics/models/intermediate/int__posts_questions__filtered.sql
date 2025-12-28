25b2435162cbf6de9217164ed97aca476d6d296d
EthanPook10 <Ethan.Pook10@outlook.com>
2025-12-28 16:26:16 +0000

    Models optimised and errors corrected

--- stack_overflow_analytics/models/intermediate/int__posts_questions__filtered.sql
+++ stack_overflow_analytics/models/intermediate/int__posts_questions__filtered.sql
@@ -1,27 +1,33 @@
-{% set month_dict = {
-   1: 'January',
-   2: 'February',
-   3: 'March',
-   4: 'April',
-   5: 'May',
-   6: 'June',
-   7: 'July',
-   8: 'August',
-   9: 'September',
-   10: 'October',
-   11: 'November',
-   12: 'December']} %}
+{% set months = [
+   'January',
+   'February',
+   'March',
+   'April',
+   'May',
+   'June',
+   'July',
+   'August',
+   'September',
+   'October',
+   'November',
+   'December'] %}
 
-with most_recent_year as (
+with unanswered_questions as (
     select *
-        from {{ ref('stg_bigquery_posts_questions') }}
+        from {{ ref('stg_bigquery__posts_questions') }}
+            where (answer_count is null or answer_count = 0)),
+
+
+most_recent_year as (
+    select *
+        from unanswered_questions
             where EXTRACT(YEAR from creation_date) = 2019), -- Would instead use = EXTRACT(YEAR FROM current_year()) if the data was up to date
 
 final as (
     select *,
         CASE EXTRACT(MONTH FROM creation_date)
-            {%- for month, month_name in month_dict.items() %}
-            when {{ month }} THEN '{{ month_name }}'
+            {%- for month in range(12) %}
+            when {{ month + 1 }} THEN '{{ months[month] }}'
             {% endfor %}
         END AS month_name
             from most_recent_year)
